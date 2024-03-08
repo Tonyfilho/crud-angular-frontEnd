004439fef55e107820275b92df03506f408cac48
tonyfilho <oficinadosdrones@gmail.com>
2024-03-08 12:52:10 +0000

    update endpoint

--- src/app/courses/services/courses.service.ts
+++ src/app/courses/services/courses.service.ts
@@ -16,7 +16,8 @@ export class CoursesService {
 
   //private readonly API = "./../../assets/_mockup_services/courses.json";
   //private readonly API = "http://localhost:8080/api/courses";
-  private readonly API = "api/courses"; //Usaremos o Proxy para resolver o error de CORRs(Corrs é feito por segurança)
+ // private readonly API = "api/courses"; //Usaremos o Proxy para resolver o error de CORRs(Corrs é feito por segurança)
+ private readonly API = "api/coursesexeption"; //mudamos o ENDPOINT para a versão mais atual do backend
 
   /**Dialog fpoi oserviço criado para abrir a PopUp de error, aqui pelo Service sem ter que ir para compoment */
   constructor(private http: HttpClient, public dialog: MatDialog) { }
